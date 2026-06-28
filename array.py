56fbd124f57fe7115d51ea166ceaa77ac330cefd
sujaydas <sujaydas17595@gmail.com>
2026-06-28 22:53:34 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,18 @@
+array=(1,2,5,6,8)
+large=(0)
+for i in range(1, len(array)):
+    if array [i]> large:
+        large = array[i]
+print(large)
+
+
+
+# arr = [10, 25, 7, 89, 45]
+
+# largest = arr[0]
+
+# for i in range(1, len(arr)):
+#     if arr[i] > largest:
+#         largest = arr[i]
+
+# print("Largest number is:", largest)
