dabbfbaa855745f233f0875c8621ce66364bebf6
sujaydas <sujaydas17595@gmail.com>
2026-07-07 00:14:08 +0530

    # find the password using loop

---
+++ real problem.py
@@ -0,0 +1,8 @@
+# problem no 1
+# find the password using loop
+
+pasword="suji"
+entee_pasword=input("enter your pasword  = ")
+while(entee_pasword!=pasword  ):
+   entee_pasword=input("wrong password! try again an enter password = ")
+print("success !your logd in ")   
