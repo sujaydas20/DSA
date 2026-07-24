f41988bbb3ec9230e900d4e5a42a88b7c819d2a0
sujaydas <sujaydas17595@gmail.com>
2026-07-25 00:57:31 +0530

    as need for hackerrank

--- hackerrank/find the runerup position.py
+++ hackerrank/find the runerup position.py
@@ -1,3 +1,6 @@
+# for my logic
+
+
 num = [6, 5, 4, 3, 2, 1]
 
 def sort(num):
@@ -5,3 +8,14 @@ def sort(num):
 
 print(sort(num))
 print(len(num) - 1)
+
+
+
+# as need for hackerrank 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    arr = list(set(arr))   
+    arr.sort()            
+    print(arr[-2])         
